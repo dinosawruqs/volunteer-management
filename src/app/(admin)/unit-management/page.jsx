445c0002367a2cede28d364acb2fdd20ec0355e5
ruqfaraway <ruqisaeful@gmail.com>
2026-06-10 00:57:36 +0700

    init awal project

---
+++ src/app/(admin)/unit-management/page.jsx
@@ -0,0 +1,10 @@
+import UnitManagementPage from '@/components/page-component/UnitManagementPage'
+import React from 'react'
+
+const page = () => {
+  return (
+    <UnitManagementPage />
+  )
+}
+
+export default page
---
+++ src/components/page-component/UnitManagementPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UnitManagementPage = () => {
+  return (
+    <div>UnitManagementPage</div>
+  )
+}
+
+export default UnitManagementPage
