f8b820486d1e10de957a1a8cb11d4a198a7b87ea
Cangkangubii <160241157+Cangkangubii@users.noreply.github.com>
2026-06-11 22:26:49 +0700

    Feature/type penugasan (#4)
    * slicing
    
    * add page

---
+++ src/components/page-component/assigment-type/add-assigment-page.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSet,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+const AddAssigmentPage = () => {
+  return (
+    <div className="flex flex-col gap-2 size-lg">
+      <h2 className="text-2xl font-bold tracking-tight">Add Assigment Type</h2>
+      <FieldSet className="w-full max-w-xs">
+        <FieldGroup>
+          <Field>
+            <FieldLabel htmlFor="username">Username</FieldLabel>
+            <Input id="username" type="text" placeholder="Max Leiter" />
+            <FieldDescription>
+              Choose a unique username for your account.
+            </FieldDescription>
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <FieldDescription>
+              Must be at least 8 characters long.
+            </FieldDescription>
+            <Input id="password" type="password" placeholder="••••••••" />
+          </Field>
+          <Field orientation="horizontal">
+            <Button type="reset" variant="outline">
+              Reset
+            </Button>
+            <Button type="submit">Submit</Button>
+          </Field>
+        </FieldGroup>
+      </FieldSet>
+    </div>
+  );
+};
+
+export default AddAssigmentPage;
