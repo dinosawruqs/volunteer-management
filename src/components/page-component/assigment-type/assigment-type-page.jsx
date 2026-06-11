2389b81775222a4ec408022202ec5c1afd09528f
Cangkangubii <ismailmubarok8@gmail.com>
2026-06-11 21:59:24 +0700

    slicing

--- src/components/page-component/assigment-type/assigment-type-page.jsx
+++ src/components/page-component/assigment-type/assigment-type-page.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button"
 
 const AssigmentTypePage = () => {
   return (
-    <div>
-      Assigment
+    <div className="flex flex-col gap-2 size-lg">
+        <h2 className="text-2xl font-bold tracking-tight">Assigment Type</h2>
+        <div className="flex items-center justify-end mb-2">
+          <Button variant="default" size="sm">
+            Add ASSIGMENT
+          </Button>
+        </div>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>NAME</TableHead>
+            <TableHead>SCORE</TableHead>
+            <TableHead className="w-[100px]">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">Ismail Mubarok</TableCell>
+            <TableCell>2000</TableCell>
+            <TableCell>
+              <Button variant="default" size="sm">
+                Detail
+              </Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
-  )
-}
+  );
+};
 
-export default AssigmentTypePage
+export default AssigmentTypePage;
